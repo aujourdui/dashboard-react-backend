cd4aa0fa3b00beb0ea022309b1e2c28937be3480
kyosuke <kyosuke.ito@gmail.com>
2022-04-08 15:31:36 -0700

    create backend part for tasks

---
+++ controllers/tasks.js
@@ -0,0 +1,10 @@
+import Task from "../models/task.js";
+
+export const getTasks = async (req, res) => {
+  try {
+    const members = await Task.find();
+    res.status(200).json(members);
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
+};
